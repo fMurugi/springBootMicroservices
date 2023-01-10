dbd6f1f948aafa3da24cd335368eb80eec305544
fMurugi <fionamurugi99@gmail.com>
2023-01-10 16:05:47 +0300

    Added hystrix and fallback method whenthe circuit breaks this is to solve the problem of microservice failling

--- Movie-catalog-service/src/main/java/com/fiona/Moviecatalogservice/Resources/MovieCatalogResource.java
+++ Movie-catalog-service/src/main/java/com/fiona/Moviecatalogservice/Resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.fiona.Moviecatalogservice.Resources;
 import com.fiona.Moviecatalogservice.Models.CatalogItem;
 import com.fiona.Moviecatalogservice.Models.Movie;
 import com.fiona.Moviecatalogservice.Models.Rating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,19 +23,24 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
     @Autowired
     RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
-//        return Collections.singletonList(
-//                new CatalogItem("Black Adam","new",4)
-//        );
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")  //this method getCatalog should break if the service is slow
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
         List<Rating> ratings = Arrays.asList(
-                new Rating("1234",4),
-                new Rating("5678",3)
-                );
-        return ratings.stream().map(rating->{
-            Movie movie = restTemplate.getForObject("http://Movie-info-service/movies/"+rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getMovieName(),"test desc", rating.getRating());
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://Movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getMovieName(), "test desc", rating.getRating());
         }).collect(Collectors.toList());
 
+    }
+
+    // fallback method is called when the getCatalog method fails or is slow
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+        return Arrays.asList(new CatalogItem("No movie", "no description", 0));
     }
 }
