9494fc3ad67e872bff5cbafe68be709ccf5f528e
fMurugi <fionamurugi99@gmail.com>
2023-01-10 11:51:49 +0300

    Added Eureka client

---
+++ Rating-data-service/src/test/java/com/fiona/Ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiona.Ratingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
